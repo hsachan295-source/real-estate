ba3ac19e183d16fb1b525086ca217bfb513b0a6e
Harsh Sachan <hsachan295@gmail.com>
2025-11-19 13:09:08 +0530

    initial clean commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+ 
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Testimonails from './components/Testimonails'
+
+const App = () => {
+  
+  return (
+    <div className='w-full overflow-hidden '>
+     
+      <Header/>
+      <About/>
+      <Projects/>
+      <Testimonails/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
+      <p className='text-gray-500 max-w-80 text-center mb-8'> Driven by Passion for Property, Committed to Your Vision</p>
+      <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'
+      >
+        <img src={assets.brand_img} className='w-full sm:w-1/2 max-w-lg' />
+        <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+          <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+            <div>
+              <p className='text-4xl font-medium text-gray-800'>10+</p>
+              <p>Year of Excellence</p>
+            </div>
+              <div>
+              <p className='text-4xl font-medium text-gray-800'>12+</p>
+              <p>Projects Completed</p>
+            </div>
+              <div>
+              <p className='text-4xl font-medium text-gray-800'>20+</p>
+              <p>Mn. Sq. Ft. Delivered</p>
+            </div>
+              <div>
+              <p className='text-4xl font-medium text-gray-800'>25+</p>
+              <p>Ongoing Projecr</p>
+            </div>
+
+          </div>
+          <p className='My-10 max-w-lg pt-4'> 
+Since 1912, we’ve built a legacy rooted in trust, excellence, and innovation. With over 10 years of modern expertise, we've delivered 20+ million sq. ft. across 12+ completed projects. Our 25+ ongoing developments continue to reflect our commitment to crafting quality spaces that stand the test of time.
+</p>
+<button className='bg-blue-600 text-white px-8 py-2 rounded-2xl'>Learn More</button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
